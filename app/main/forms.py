bd0d35a4c89a637929c795e5149d5d91382c2bfa
William Owalla <william.owalla@student.moringaschool.com>
2022-05-10 19:17:47 +0300

    modify index

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import InputRequired
+from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField
+from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import User
 
 class PitchForm(FlaskForm):
