0413acd2933a2799a635ed1953b24f644ae254dc
William Owalla <william.owalla@student.moringaschool.com>
2022-05-10 17:29:25 +0300

    initialize flask extentions

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+simple = SimpleMDE()
 
 
 def create_app(config_name):
@@ -8,6 +22,13 @@ def create_app(config_name):
 
     #app configurations
     app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
