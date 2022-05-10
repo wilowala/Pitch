1fb3bf2e737cf4c2a0d62c4d8aa7a11031ac4cd3
William Owalla <william.owalla@student.moringaschool.com>
2022-05-10 18:44:14 +0300

    add email and simple mde configurations

--- config.py
+++ config.py
@@ -1,6 +1,21 @@
+import os
+import psycopg2
+
 class Config:
     """Parent Config Class """
-    
+    SECRET_KEY = os.urandom(112139)
+    SQLALCHEMY_DATABASE_URI ="postgresql+psycopg2://username:aivwilie@localhost/pitch"
+
+     #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
     """Production Config Class"""
