cb35b5d49db05785620b48cf0e2ed1571bb89f0a
William Owalla <william.owalla@student.moringaschool.com>
2022-05-10 17:30:44 +0300

    initiate user, pitch, and role models

---
+++ app/models.py
@@ -0,0 +1,72 @@
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+
+class Pitch(db.Model):
+
+    all_pitches = []
+
+    def __init__(self,title,author,pitch):
+        self.title = title
+        self.author = author
+        self.pitch = pitch
+
+
+    def save_pitch(self):
+        Pitch.all_pitches.append(self)
+
+
+    @classmethod
+    def clear_pitch(cls):
+        Pitch.all_pitches.clear()
+
+    @classmethod
+    def get_pitch(cls,id):
+
+        response = []
+
+        for pitch in cls.all_pitches:
+            if pitch.movie_id == id:
+                response.append(pitch)
+
+        return response
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
