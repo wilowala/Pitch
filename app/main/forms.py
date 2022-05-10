50222db064b0dffe281359993ef215ff3b00334a
William Owalla <william.owalla@student.moringaschool.com>
2022-05-10 17:28:33 +0300

    add registration and login classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import InputRequired
+from ..models import User
 
 class PitchForm(FlaskForm):
 
@@ -8,3 +9,24 @@ class PitchForm(FlaskForm):
     title = StringField('Pitch title',validators=[InputRequired()])
     pitch = TextAreaField('Pitch')
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    password = PasswordField('Password',validators =[InputRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    username = StringField('Enter your username',validators = [InputRequired()])
+    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
